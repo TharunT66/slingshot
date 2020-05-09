0c85056a6298985f8a0413acf5452ebcfdd50d13
unknown <tapzap18@gmail.com>
2020-05-09 16:50:45 +0530

    added slingshot effect to the angry bird game

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,10 +35,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-    log6 = new Log(270,180,150, -PI/7);
+    
 
     bird = new Bird(100,100);
-chain = new Chain(bird.body,log6.body);
+slingshot = new Slingshot(bird.body,{x:200,y:100});
 
 
 }
@@ -63,9 +63,16 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-    log6.display();
 
     bird.display();
     platform.display();
-    chain.display();
+    slingshot.display();
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+    slingshot.fly();
 }
